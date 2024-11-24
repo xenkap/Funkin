0277f5c58254d6b781b3c0a30a648fa53ee57777
zackaryowo <103583353+xenkap@users.noreply.github.com>
2024-11-24 15:40:17 +0800

    Revert "change ResyncVocals to use FrameSongPos"
    This reverts commit 1f933e808acd0ba453f43699a65ec9332efa6ca7.
    hmmm

--- source/funkin/play/PlayState.hx
+++ source/funkin/play/PlayState.hx
@@ -2128,8 +2128,7 @@ class PlayState extends MusicBeatSubState
     // Skip this if the music is paused (GameOver, Pause menu, start-of-song offset, etc.)
     if (!(FlxG.sound.music?.playing ?? false)) return;
 
-    var timeToPlayAt:Float = Math.min(FlxG.sound.music.length, Math.max(Math.min(Conductor.instance.combinedOffset, 0),
-                                      Conductor.instance.frameSongPosition) - Conductor.instance.combinedOffset);
+    var timeToPlayAt:Float = Math.min(FlxG.sound.music.length, Math.max(Math.min(Conductor.instance.combinedOffset, 0), Conductor.instance.songPosition) - Conductor.instance.combinedOffset);
     trace('Resyncing vocals to ${timeToPlayAt}');
 
     FlxG.sound.music.pause();
